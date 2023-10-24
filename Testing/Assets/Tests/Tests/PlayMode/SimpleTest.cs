efb5e467dc063548566a43c6b2e538a67d218cfd
Moromon <lmoralvxd@gmail.com>
2023-10-24 11:28:26 +0200

    Proyecto

---
+++ Testing/Assets/Tests/Tests/PlayMode/SimpleTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class SimpleTest
+{
+    [Test]
+
+    public void SimpleTestSimplePasses()
+    {
+        Assert.AreEqual(2+2, 4);
+    }
+
+}
